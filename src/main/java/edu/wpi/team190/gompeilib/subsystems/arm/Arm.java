1989765d1ed5d16f657f65d5cbfacb55deef7bcd
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2026-01-12 20:44:51 -0500

    Arm IO and Constants

--- src/main/java/edu/wpi/team190/gompeilib/subsystems/arm/Arm.java
+++ src/main/java/edu/wpi/team190/gompeilib/subsystems/arm/Arm.java
@@ -1,4 +1,24 @@
 package edu.wpi.team190.gompeilib.subsystems.arm;
 
-public class Arm {
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+    public ArmIO io;
+    public ArmConstants armConstants;
+    public ArmIOInputsAutoLogged inputs;
+
+    private boolean isClosedLoop;
+
+    private double position;
+    private double positionGoalMeters;
+
+    public Arm(ArmConstants constants, ArmIO io) {
+        this.io = io;
+        this.armConstants = constants;
+        this.inputs = new ArmIOInputsAutoLogged();
+    }
+
+    
+
+
 }
--- src/main/java/edu/wpi/team190/gompeilib/subsystems/arm/ArmIO.java
+++ src/main/java/edu/wpi/team190/gompeilib/subsystems/arm/ArmIO.java
@@ -1,4 +1,44 @@
 package edu.wpi.team190.gompeilib.subsystems.arm;
 
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.team190.gompeilib.core.utility.GainSlot;
+
 public interface ArmIO {
+    @AutoLog
+    public static class ArmIOInputs {
+        public Rotation2d armPosition = new Rotation2d();
+
+        public double armVelocityRadiansPerSecond = 0.0;
+        public double accelerationRadiansPerSecondSquared = 0.0;
+
+        public double[] armAppliedVolts = new double[] {};
+        public double[] armSupplyCurrentAmps = new double[] {};
+        public double[] armTorqueCurrentAmps = new double[] {};
+        public double[] armTemperatureCelsius = new double[] {};
+
+        public Rotation2d armPositionGoal = new Rotation2d();
+        public Rotation2d armPositionSetpoint = new Rotation2d();
+        public Rotation2d armPositionError = new Rotation2d();
+    }
+
+  public default void updateInputs(ArmIOInputs inputs) {}
+
+  public default void setArmVoltage(double volts) {}
+
+  public void setPosition(Rotation2d position);
+
+  public default void setArmPositionGoal(Rotation2d positionGoal) {}
+
+  public void updateGains(double kP, double kD, double kS, double kV, double kA, double kG);
+  
+  public void setPositionGoal(double positionMeters, GainSlot slot);
+  public void updateGains(
+      double kP, double kD, double kS, double kV, double kA, double kG, GainSlot slot);
+
+  public default void updateArmConstraints(double maxAcceleration, double cruisingVelocity) {}
+
+  public default void zeroArmPosition() {}
+
 }
