1731856929c9985f7f317bd51fb584fb095e3eb0
ElliotScher <ecscher84@gmail.com>
2026-01-15 17:47:44 -0500

    Fix example project

--- examples/swerve/src/main/java/frc/robot/BuildConstants.java
+++ examples/swerve/src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "swerve";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 157;
-  public static final String GIT_SHA = "860407ffb3c848bfb1aca2beed7e6f0d4e5a8997";
-  public static final String GIT_DATE = "2026-01-14 23:30:48 EST";
+  public static final int GIT_REVISION = 164;
+  public static final String GIT_SHA = "5de79caa89bc501ed3f6c94a05c31bda41fba730";
+  public static final String GIT_DATE = "2026-01-15 17:45:00 EST";
   public static final String GIT_BRANCH = "development";
-  public static final String BUILD_DATE = "2026-01-15 00:47:01 EST";
-  public static final long BUILD_UNIX_TIME = 1768456021700L;
+  public static final String BUILD_DATE = "2026-01-15 17:45:57 EST";
+  public static final long BUILD_UNIX_TIME = 1768517157736L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
