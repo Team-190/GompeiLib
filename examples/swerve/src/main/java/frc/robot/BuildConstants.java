c16e7313ddabe31e336622d5ba7cac8c95ef739c
PaarkG <2007kdogrules@gmail.com>
2026-01-15 19:50:22 -0500

    run autoformatting

--- examples/swerve/src/main/java/frc/robot/BuildConstants.java
+++ examples/swerve/src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "swerve";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 176;
-  public static final String GIT_SHA = "510f96a5b866bdd7aceec5d3a343698e0944381a";
-  public static final String GIT_DATE = "2026-01-15 19:28:39 EST";
+  public static final int GIT_REVISION = 177;
+  public static final String GIT_SHA = "035da5114d1106dd151b5b14c90bcd31b25b21b6";
+  public static final String GIT_DATE = "2026-01-15 19:34:34 EST";
   public static final String GIT_BRANCH = "arm-arnav-ananth";
-  public static final String BUILD_DATE = "2026-01-15 19:29:06 EST";
-  public static final long BUILD_UNIX_TIME = 1768523346201L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2026-01-15 19:49:21 EST";
+  public static final long BUILD_UNIX_TIME = 1768524561036L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
