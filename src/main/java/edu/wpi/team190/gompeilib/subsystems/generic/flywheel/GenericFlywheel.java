ea5fa5704e18069d90457067440c5c35f7bfb096
PaarkG <2007kdogrules@gmail.com>
2026-01-17 16:18:23 -0500

    updated flywheel to not have wasteful code and call io properly

--- src/main/java/edu/wpi/team190/gompeilib/subsystems/generic/flywheel/GenericFlywheel.java
+++ src/main/java/edu/wpi/team190/gompeilib/subsystems/generic/flywheel/GenericFlywheel.java
@@ -10,45 +10,26 @@ public class GenericFlywheel {
   private final GenericFlywheelIO io;
   private final GenericFlywheelIOInputsAutoLogged inputs;
 
-  private final int index;
   private final String aKitTopic;
-
-  private double velocityGoalRadiansPerSecond;
-  private double voltageGoalVolts;
-
-  private boolean isClosedLoop;
 
   public GenericFlywheel(GenericFlywheelIO io, Subsystem subsystem, int index) {
     this.io = io;
     inputs = new GenericFlywheelIOInputsAutoLogged();
 
-    this.index = index;
     aKitTopic = subsystem.getName() + "/Flywheels" + index;
-
-    velocityGoalRadiansPerSecond = 0;
-    voltageGoalVolts = 0;
-
-    isClosedLoop = true;
   }
 
   public void periodic() {
     io.updateInputs(inputs);
     Logger.processInputs(aKitTopic, inputs);
-    if (isClosedLoop) {
-      io.setVelocity(velocityGoalRadiansPerSecond);
-    } else {
-      io.setVoltage(voltageGoalVolts);
-    }
   }
 
   public void setGoal(double velocityGoalRadiansPerSecond) {
-    isClosedLoop = true;
-    this.velocityGoalRadiansPerSecond = velocityGoalRadiansPerSecond;
+    io.setVelocity(velocityGoalRadiansPerSecond);
   }
 
   public void setVoltage(double volts) {
-    isClosedLoop = false;
-    this.voltageGoalVolts = volts;
+      io.setVelocity(volts);
   }
 
   public boolean atGoal() {
