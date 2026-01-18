9022a9923090581e66b583bac0a22943917b7231
Anshu Adiga <aadiga@wpi.edu>
2026-01-18 14:06:53 -0500

    BuildConstants:VERSION to the gompeilib version

--- examples/swerve/src/main/java/frc/robot/BuildConstants.java
+++ examples/swerve/src/main/java/frc/robot/BuildConstants.java
@@ -4,13 +4,13 @@ package frc.robot;
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "swerve";
-  public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 164;
-  public static final String GIT_SHA = "5de79caa89bc501ed3f6c94a05c31bda41fba730";
-  public static final String GIT_DATE = "2026-01-15 17:45:00 EST";
-  public static final String GIT_BRANCH = "development";
-  public static final String BUILD_DATE = "2026-01-15 17:45:57 EST";
-  public static final long BUILD_UNIX_TIME = 1768517157736L;
+  public static final String VERSION = "GompeiLib:github-packages:2026.2.13";
+  public static final int GIT_REVISION = 168;
+  public static final String GIT_SHA = "cf1d0af5238df271efc7355fec130ad03b9783b7";
+  public static final String GIT_DATE = "2026-01-15 20:45:26 EST";
+  public static final String GIT_BRANCH = "jitpack-fallback";
+  public static final String BUILD_DATE = "2026-01-18 14:05:21 EST";
+  public static final long BUILD_UNIX_TIME = 1768763121437L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
