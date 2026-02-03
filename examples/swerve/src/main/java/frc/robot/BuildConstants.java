ee94d805aba4e8461a93479de5335e01e0ce8b4d
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2026-02-02 20:50:33 -0500

    arm

---
+++ examples/swerve/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "swerve";
+  public static final String VERSION = "GompeiLib:github-packages:2026.2.1";
+  public static final int GIT_REVISION = 264;
+  public static final String GIT_SHA = "3198d9d2b0673578dce487aa59525ac767eaee8b";
+  public static final String GIT_DATE = "2026-01-31 20:45:39 EST";
+  public static final String GIT_BRANCH = "development";
+  public static final String BUILD_DATE = "2026-02-02 20:50:00 EST";
+  public static final long BUILD_UNIX_TIME = 1770083400511L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
