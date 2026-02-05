8494683bcef49ede01e07338e6be5e70ae5fc95e
vaddadisri26 <vaddadi.sriaditya@gmail.com>
2026-02-05 18:44:53 -0500

    Changed ON_CANIVORE to take in a CANBus object
    Co-authored-by: SeptimusHeap7 <SeptimusHeap7@users.noreply.github.com>
    Co-authored-by: Abhiraam Venigalla <Abhi52897340598327@users.noreply.github.com>

--- src/main/java/edu/wpi/team190/gompeilib/subsystems/generic/roller/GenericRollerConstants.java
+++ src/main/java/edu/wpi/team190/gompeilib/subsystems/generic/roller/GenericRollerConstants.java
@@ -1,5 +1,7 @@
 package edu.wpi.team190.gompeilib.subsystems.generic.roller;
 
+import com.ctre.phoenix6.CANBus;
+
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.MomentOfInertia;
 import lombok.RequiredArgsConstructor;
@@ -11,5 +13,5 @@ public class GenericRollerConstants {
   public final DCMotor ROLLER_GEARBOX;
   public final double ROLLER_MOTOR_GEAR_RATIO;
   public final MomentOfInertia MOMENT_OF_INERTIA;
-  public final boolean ON_CANIVORE;
+  public final CANBus ON_CANIVORE;
 }
