50ea9d1dc124b8b8d3205ba74c665cab407c15fe
ElliotScher <ecscher84@gmail.com>
2026-02-15 14:39:13 -0500

    follwer motors for roller.

--- src/main/java/edu/wpi/team190/gompeilib/subsystems/generic/roller/GenericRollerConstants.java
+++ src/main/java/edu/wpi/team190/gompeilib/subsystems/generic/roller/GenericRollerConstants.java
@@ -5,17 +5,20 @@ import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.MomentOfInertia;
+import java.util.Set;
 import lombok.Builder;
 import lombok.NonNull;
 
 @Builder(setterPrefix = "with")
 public class GenericRollerConstants {
-  @NonNull public final Integer rollerCANID;
+  @NonNull public final Integer leaderCANID;
+  @NonNull public final InvertedValue leaderInvertedValue;
+  @NonNull public final Set<Integer> alignedFollowerCANIDs;
+  @NonNull public final Set<Integer> opposedFollowerCANIDs;
   @NonNull public final Double supplyCurrentLimit;
   @NonNull public final DCMotor rollerGearbox;
   @NonNull public final Double rollerMotorGearRatio;
   @NonNull public final MomentOfInertia momentOfInertia;
   @NonNull public final NeutralModeValue neutralMode;
   @NonNull public final CANBus canBus;
-  @NonNull public final InvertedValue invertedValue;
 }
