49d364b6e1ca3556b1eb6fbe10c38be88fd7de5a
Anshu Adiga <aadiga@wpi.edu>
2026-02-24 17:52:17 -0500

    LTM helper methods and correct string formatting

--- src/main/java/edu/wpi/team190/gompeilib/core/utility/control/AngularConstraints.java
+++ src/main/java/edu/wpi/team190/gompeilib/core/utility/control/AngularConstraints.java
@@ -1,7 +1,6 @@
 package edu.wpi.team190.gompeilib.core.utility.control;
 
 import edu.wpi.first.units.*;
-import edu.wpi.first.units.measure.Angle;
 import edu.wpi.team190.gompeilib.core.utility.tunable.LoggedTunableMeasure;
 import lombok.Builder;
 import lombok.NonNull;
@@ -16,16 +15,12 @@ public record AngularConstraints(
   @Builder(setterPrefix = "with")
   public AngularConstraints(
       @NonNull String prefix,
-      @NonNull Angle baseUnit,
       Measure<AngleUnit> goalTolerance,
       Measure<AngularVelocityUnit> maxVelocity,
       Measure<AngularAccelerationUnit> maxAcceleration) {
     this(
-        new LoggedTunableMeasure<>(
-            String.format("%s/Goal Tolerance (%s)", prefix, baseUnit), goalTolerance),
-        new LoggedTunableMeasure<>(
-            String.format("%s/Max Velocity (%s/s)", prefix, baseUnit), maxVelocity),
-        new LoggedTunableMeasure<>(
-            String.format("%s/Max Acceleration (%s/s^2)", prefix, baseUnit), maxAcceleration));
+        new LoggedTunableMeasure<>(String.format("%s/Goal Tolerance", prefix), goalTolerance),
+        new LoggedTunableMeasure<>(String.format("%s/Max Velocity", prefix), maxVelocity),
+        new LoggedTunableMeasure<>(String.format("%s/Max Acceleration", prefix), maxAcceleration));
   }
 }
